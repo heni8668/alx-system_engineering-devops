7412462bab619aff70b8833bdd6c7da0d501b996
heni8668 <henoktilahun363@gmail.com>
2022-09-16 07:48:50 -0700

    new

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# Puppet manifests that install puppet-lint
+# install puppet-lint
 package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem',
-  source   => 'https://rubygems.org',
+  ensure   => '2.1.1',
+  provider => 'gem'
 }
+
